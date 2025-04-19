212aff3b453e7c59e97aeed03ede6d146c05ba39
osamanadeem8 <osama.nadeem@kodifly.com>
2025-04-20 03:02:00 +0500

    Added new functions, added another package

---
+++ greetings/hello.go
@@ -0,0 +1,7 @@
+package greetings
+
+import "fmt"
+
+func HelloGreet(name string) string {
+	return fmt.Sprintf("HEEEELLLLOOOOO %v", name)
+}
--- hello/main.go
+++ hello/main.go
@@ -1,9 +1,27 @@
 package main
 
-import ("fmt")
-import ("rsc.io/quote")
+import (
+	"fmt"
+	"rsc.io/quote"
+	"example.com/greetings"
+)
 
 func main() {
 	fmt.Println("Hello World")
-	fmt.Println(quote.Hello())
+	fmt.Println(quote.Go())
+
+	fmt.Println(Hello("osama"))
+	fmt.Println(Hello2("osama"))
+	fmt.Println(greetings.HelloGreet("osama"))
+}
+
+func Hello(name string) string {
+	message:= fmt.Sprintf("This is %v", name)
+	return message
+}
+
+func Hello2(name string) string {
+	var message string
+	message = fmt.Sprintf("This is %v", name)
+	return message
 }
